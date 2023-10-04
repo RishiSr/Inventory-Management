b75ca741e20ff5a91abccd07bec0648b782d3efe
rishi <rishisr.9044@gmail.com>
2023-10-05 01:14:08 +0530

    Changed the cors address

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://inventory-mng-rishi.onrender.com/"],
+    origin: ["http://localhost:3000", "https://inventory-mng-rishi.onrender.com"],
     credentials: true,
   })
 );
